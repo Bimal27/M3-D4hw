e6934087450212e397a0efa4c41fb9372b6fa5af
Bimal27 <bimalsah1996@gmail.com>
2021-08-12 15:21:31 +0200

    ex 2 done

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-
+const displayBooks = (books) =>{
+    let row = document.querySelector('.row')
+    books.forEach((book) =>{
+        row.innerHTML +=`<div class="col col-sm-12 col-md-4 col-lg-3">
+                            <div class="card">
+                                <img src="${book.img}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                <h5 class="card-title">${book.title}</h5>
+                                
+                                <a href="#" class="btn btn-primary">${book.price}</a>
+                                </div>
+                            </div>
+                        </div>`
+    })
+}
 
 const fetchData = () =>{
     fetch('https://striveschool-api.herokuapp.com/books')
     .then(resp => resp.json())
     .then(books =>{
+        displayBooks(books)
         console.log(books)
     }).catch(err =>{
         console.log(err)
