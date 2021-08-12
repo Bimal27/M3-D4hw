2ae2aa362fd01811d96329a4ee37bd5b025b6d44
Bimal27 <bimalsah1996@gmail.com>
2021-08-12 15:38:12 +0200

    exercise 4 and 5 done

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+
+const skipMe = (e) => {
+    e.target.closest('.col-md-4').remove()
+}
+
 const displayBooks = (books) =>{
     let row = document.querySelector('.row')
     books.forEach((book) =>{
-        row.innerHTML +=`<div class="col col-sm-12 col-md-4 col-lg-3">
+        row.innerHTML +=`<div class="col-md-4">
                             <div class="card">
                                 <img src="${book.img}" class="card-img-top" alt="...">
                                 <div class="card-body">
                                 <h5 class="card-title">${book.title}</h5>
                                 
-                                <a href="#" class="btn btn-primary">${book.price}</a>
+                                <a href="#" class="btn btn-primary mx-1">${book.price}</a>
+
+                                <a href="#" class="btn btn-success">Cart</a>
+                                <button class="btn btn-warning mx-1" onclick="skipMe(event)">Skip</button>
+                               
                                 </div>
                             </div>
                         </div>`
