47b407be42a8029465c83bb7562b71f5c68ade00
Bimal27 <bimalsah1996@gmail.com>
2021-08-12 16:30:47 +0200

    updated

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+ let bookList = []
+
+
+ function select(e) {
+     e.target.closest('.card').classList.add("selected")
+ }
+
+ function filterBooks(query) {
+    console.log(query)
+    const filteredBooks = bookList.filter(book => book.title.toLowerCase().includes(query.toLowerCase()))
+    displayBooks(filteredBooks)
+}
 
 const skipMe = (e) => {
     e.target.closest('.col-md-4').remove()
@@ -12,9 +24,9 @@ const displayBooks = (books) =>{
                                 <div class="card-body">
                                 <h5 class="card-title">${book.title}</h5>
                                 
-                                <a href="#" class="btn btn-primary mx-1">${book.price}</a>
+                                <badge class="btn btn-primary mx-1">${book.price}</badge>
 
-                                <a href="#" class="btn btn-success">Cart</a>
+                                <button class="btn btn-success" onclick="select(event)">Cart</button>
                                 <button class="btn btn-warning mx-1" onclick="skipMe(event)">Skip</button>
                                
                                 </div>
@@ -27,6 +39,8 @@ const fetchData = () =>{
     fetch('https://striveschool-api.herokuapp.com/books')
     .then(resp => resp.json())
     .then(books =>{
+        bookList = books
+       
         displayBooks(books)
         console.log(books)
     }).catch(err =>{
